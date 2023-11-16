a8b2a8efc409891e415712cb6d35d233a6ca728e
Victor Campos <cravcampos@gmail.com>
2023-11-16 16:02:58 -0500

    ejemplo de que se puede hacer con JS

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -1,1 +1,8 @@
-alert("Hola Mundo");
+// Leccion 1 creando archivo independiente de JS
+// alert("Hola Mundo");
+
+// Que se puede hacer con JS
+const nombre = prompt("Cual es tu Nombre");
+document.querySelector(
+  ".contenido"
+).innerHTML = `${nombre} esta aprendiendo JS`;
