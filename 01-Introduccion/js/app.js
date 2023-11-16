97449263cb3b19c5c85cbf4238f008c244cf7d03
Victor Campos <cravcampos@gmail.com>
2023-11-16 16:05:16 -0500

    Ejemplo de comentarios de codigo en JS

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -2,7 +2,12 @@
 // alert("Hola Mundo");
 
 // Que se puede hacer con JS
+/*
 const nombre = prompt("Cual es tu Nombre");
 document.querySelector(
   ".contenido"
 ).innerHTML = `${nombre} esta aprendiendo JS`;
+*/
+
+// forma de comentar codigo en linea en JS
+/* forma de comentar codigo en bloque en JS */
