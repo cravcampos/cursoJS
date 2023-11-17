03a3cb80c35cd9f8140850d1ff7380c7455108c4
Victor Campos <cravcampos@gmail.com>
2023-11-17 02:26:37 -0500

    ejemplo de uso de variables const

---
+++ 02-variables/js/03-app.js
@@ -0,0 +1,13 @@
+// la tercera forma de crear variables es const
+// existen pocas diferencias entre const y let, asi que vamos a verlas
+// Primero una variable const su valor no puede ser reasignado.
+
+const producto = "Monitor 30 pulgadas";
+console.log(producto);
+producto = "Monitor 27 pulgadas"; // NO se puede reasignar la variable
+console.log(producto);
+
+// Las variables const deben iniciarse con un valor
+//const precio; //Genera un error
+precio = 20;
+console.log(precio);
