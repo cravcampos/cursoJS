f2426720f0b3f62de116c4f9a874faf1e0c43ac6
Victor Campos <cravcampos@gmail.com>
2023-11-16 23:46:25 -0500

    Ejemplos de uso de la consola de JS

--- 01-Introduccion/js/app.js
+++ 01-Introduccion/js/app.js
@@ -11,3 +11,44 @@ document.querySelector(
 
 // forma de comentar codigo en linea en JS
 /* forma de comentar codigo en bloque en JS */
+
+// La consola de JS
+// Se pueden crear varibles e imprimirlas
+const hola = "Hola desde la consola";
+console.log(hola);
+
+// Crear arreglos desde la consola
+console.log(["1,2,3,4"]);
+
+// Crear objetos desde la consola
+let obj = {
+  nombre: "Victor",
+  apellido: "Campos",
+};
+console.log(obj);
+
+// Mostrar resultados como una tabla
+console.table([1, 2, 3, 4, 5]);
+
+// Mostrar un erros desde la consola
+console.error("Este es un mensaje de error");
+
+// Mostrar un mensaje de advertencia desde la consola
+console.warn("Esto es un mensaje de advertencia");
+
+// Limpiar la consola
+//console.clear();
+
+// monitorear el tiempo que tarda en ejecutarse algo
+console.time("Hola");
+console.warn("Esto no esta permitido");
+console.warn("Esto no esta permitido");
+console.warn("Esto no esta permitido");
+console.warn("Esto no esta permitido");
+console.warn("Esto no esta permitido");
+console.warn("Esto no esta permitido");
+console.warn("Esto no esta permitido");
+console.warn("Esto no esta permitido");
+console.warn("Esto no esta permitido");
+console.warn("Esto no esta permitido");
+console.timeEnd("Hola");
