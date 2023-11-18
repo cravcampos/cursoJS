17c602bb2ea2f2c314ae85029c48ef9350e71ca5
Victor Campos <cravcampos@gmail.com>
2023-11-17 22:17:53 -0500

    Ejemplo de como crear variables numericas en JS

---
+++ 04-numeros/js/01-app.js
@@ -0,0 +1,22 @@
+// En la consola se ven los numeros de diferente color
+const numero = 34;
+const numero1 = "34";
+console.log(numero);
+console.log(numero1);
+
+// Forma de crear variables numericas en JS
+const numero2 = 30;
+const numero3 = 20;
+const numero4 = 20.2;
+const numero5 = 0.10213;
+const numero6 = -3;
+
+console.log(numero2);
+console.log(numero3);
+console.log(numero4);
+console.log(numero5);
+console.log(numero6);
+
+// Otra forma de crear numeros en JS
+const numero7 = new Number(610);
+console.log(numero7);
