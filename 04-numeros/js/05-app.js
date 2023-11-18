6554216afba860b198a3ad22808e69988bb0809d
Victor Campos <cravcampos@gmail.com>
2023-11-18 09:59:22 -0500

    ejemplo de uso de operadores de incremento y decremento

---
+++ 04-numeros/js/05-app.js
@@ -0,0 +1,36 @@
+// Operadores de incremento y decremento
+
+// incremento
+let puntaje = 5;
+console.log(puntaje);
+// incrementa luego de llamar la funcion
+puntaje++;
+console.log(puntaje);
+
+// incrementa y luego llama la funcion
+++puntaje;
+console.log(puntaje);
+
+// decrementa luego llama la funcion
+puntaje--;
+console.log(puntaje);
+
+// decrementa y luego llama la funcion
+--puntaje;
+console.log(puntaje);
+
+// formas de sumar el resultado por un numero
+puntaje += 3;
+console.log(puntaje);
+
+// formas de restar el resultado por un numero
+puntaje -= 3;
+console.log(puntaje);
+
+// formas de multiplicar el resultado por un numero
+puntaje *= 5;
+console.log(puntaje);
+
+// formas de dividir el resultado por un numero
+puntaje /= 5;
+console.log(puntaje);
