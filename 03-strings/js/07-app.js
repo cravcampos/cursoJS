846f70c8e3c6b40ea338634282e57c520406cbfd
Victor Campos <cravcampos@gmail.com>
2023-11-17 21:22:36 -0500

    ejemplo de uso de toLowerCase(), toUpperCase() y toString()

---
+++ 03-strings/js/07-app.js
@@ -0,0 +1,12 @@
+const producto = "Monitor de 29 pulgadas";
+
+// toUpperCase() permite cambiar un texto todo a mayusculas
+console.log(producto.toUpperCase());
+
+// toLowerCase() permite pasar todo el texto a minusculas
+console.log(producto.toLowerCase());
+
+// toString() pasar una variable a tipo string
+const precio = 2000;
+console.log(precio);
+console.log(precio.toString());
