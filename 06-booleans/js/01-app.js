d8616e481dec3fedc725791c888e8535114f8dff
Victor Campos <cravcampos@gmail.com>
2023-11-18 13:04:16 -0500

    ejemplo de creacion de variables boolean

---
+++ 06-booleans/js/01-app.js
@@ -0,0 +1,15 @@
+// el tipo de ddato boolean solo puede tener dos valores true o false
+
+const boolean1 = true;
+const boolean2 = false;
+
+console.log(boolean1);
+console.log(boolean2);
+
+// Verificar el tipo de dato
+console.log(typeof boolean2);
+
+// Tambien se puede crear boolean con la palabra new
+const boolean3 = new Boolean(true);
+console.log(boolean3);
+console.log(typeof boolean3); // al crearlo de esta forma nos dice q es un objeto
