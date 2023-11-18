5932252c938083863997bfdbe77f451db53b63ff
Victor Campos <cravcampos@gmail.com>
2023-11-17 22:23:41 -0500

    Ejemplon de uso de los operadores + - * / % en JS

---
+++ 04-numeros/js/02-app.js
@@ -0,0 +1,26 @@
+const numero = 30;
+const numero1 = 20;
+
+let resultado;
+
+// se conocen como operadores, sirven para realizar sumas, restas, multiplicaciones, divisiones, modulos
+
+// Suma +
+resultado = numero + numero1;
+console.log(resultado);
+
+// Resta -
+resultado = numero - numero1;
+console.log(resultado);
+
+// Multiplicación *
+resultado = numero * numero1;
+console.log(resultado);
+
+// Division /
+resultado = numero / numero1;
+console.log(resultado);
+
+// Modulo %
+resultado = numero % numero1;
+console.log(resultado);
