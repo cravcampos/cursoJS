291aee1f272f0582f3fca56e5e3b5dd520b9658a
Victor Campos <cravcampos@gmail.com>
2023-11-18 12:19:32 -0500

    ejemplo de oparadores de comparacion == === != !==

---
+++ 05-operadores/js/02-app.js
@@ -0,0 +1,19 @@
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = 30;
+
+// revisar si 2 valores son iguales
+console.log(numero1 == numero3);
+console.log(numero1 == numero2); // solo se fija en el valor
+
+// Operador estricto(revisar el valor y tipo de dato)
+console.log(numero1 === numero2);
+
+// Saber de que tipo es la variable
+console.log(typeof numero1);
+console.log(typeof numero2);
+
+// Diferente a
+console.log(numero1 != numero3);
+console.log(numero1 != numero2);
+console.log(numero1 !== numero2); // compara valor y tipo de dato
