b77072a4b57678c7ae2f7773f0dcc7b34cd68e3f
Victor Campos <cravcampos@gmail.com>
2023-11-19 21:58:26 -0500

    iterar un arreglo con el ciclo for

---
+++ 08-arreglos/js/03-app.js
@@ -0,0 +1,10 @@
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio"];
+console.table(meses);
+
+// Saber el tamaño de un arreglo
+console.log(meses.length);
+
+// Iterar el arreglo usando for
+for (let i = 0; i < meses.length; i++) {
+  console.log(meses[i]);
+}
