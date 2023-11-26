ee3dc492933daa7acc683ac2ef968a692e41ebea
Victor Campos <cravcampos@gmail.com>
2023-11-25 22:55:09 -0500

    diferencias entre funcion y metodo en JS

---
+++ 09-funciones/js/04-app.js
@@ -0,0 +1,15 @@
+// Diferencia entre metodos y funciones
+const numero1 = 20;
+const numero2 = "20";
+
+// En realidad terminan siendo lo mismo la forma en la que se utilizan tiene que ver mas que nada
+// en el contexto que son utilizadas
+
+console.log(parseInt(numero2)); // esto es una funcion
+console.log(numero1.toString()); // esto es un metodo
+
+// los metodos son paret de uan clase, es decir es parte de la funcionalidad que se le da a un
+// objeto
+
+// mientras una funcion es un bloque de codigo independiente que puede ser llamado en cualquier
+// parte del programa
