cf8bdf221543384ee614c7c7097941081f0be003
Victor Campos <cravcampos@gmail.com>
2023-11-30 19:53:16 -0500

    ejemplo de uso de >, >=, < y <=

---
+++ 10-estructurascontrol/js/03-app.js
@@ -0,0 +1,14 @@
+const dinero = 1600;
+const totalPagar = 300;
+
+if (dinero >= totalPagar) {
+  console.log("Si se puede pagar");
+} else {
+  console.log("Fondos Insuficientes");
+}
+
+if (dinero <= totalPagar) {
+  console.log("Si se puede pagar");
+} else {
+  console.log("Fondos Insuficientes");
+}
