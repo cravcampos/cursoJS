f20202fb937035b40fed171f617aa930df0aff5c
Victor Campos <cravcampos@gmail.com>
2023-11-30 19:14:35 -0500

    ejemplo de uso operadores !=, !==, == y ===

---
+++ 10-estructurascontrol/js/02-app.js
@@ -0,0 +1,23 @@
+const puntaje = 1000;
+
+if (puntaje != 1000) {
+  // operdor diferente !=
+  console.log("Si es diferente!!!");
+} else {
+  console.log("No es diferente!!!");
+}
+
+if (puntaje === "1000") {
+  // operador estricto en JS ===
+  console.log("Si es igual!!!");
+} else {
+  console.log("No es igual!!!");
+}
+
+if (puntaje !== 1000) {
+  // operdor diferente estricto !==
+  // operdor diferente !=
+  console.log("Si es diferente!!!");
+} else {
+  console.log("No es diferente!!!");
+}
