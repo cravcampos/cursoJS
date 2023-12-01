f8c20e2f64fe32d82cbb575967d096edca15bf41
Victor Campos <cravcampos@gmail.com>
2023-11-30 19:07:36 -0500

    ejemplo de uso de if else

---
+++ 10-estructurascontrol/js/01-app.js
@@ -0,0 +1,11 @@
+// las estructuras de control, se utilizan para definir que parte del codigo deseamos que se ejecute basados en una condición
+
+// Operador If
+
+const puntaje = "1000";
+
+if (puntaje == 1000) {
+  console.log("Si es igual!!!"); // Si la condicion se cumple se ejecuta esto
+} else {
+  console.log("No es igual!!!"); // Si la condicion no se cumple ejecuta esto
+}
