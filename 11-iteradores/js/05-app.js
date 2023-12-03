a932d3ffa32fbdc371dde7a8a87f39d55269bd4f
Victor Campos <cravcampos@gmail.com>
2023-12-03 18:26:25 -0500

    ejemplo de uso del ciclo do while

---
+++ 11-iteradores/js/05-app.js
@@ -0,0 +1,10 @@
+// Ciclo do while
+// A difrencia del while este se ejecuta al menos una vez se cumpla o no la condicion
+// Funciona de la misma manera que el while
+
+let i = 10;
+
+do {
+  console.log(`Numero: ${i}`);
+  i++;
+} while (i < 10); // Condicion
