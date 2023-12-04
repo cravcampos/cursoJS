816b4f9036e4450445c5099bed4aba91ee1a5ac5
Victor Campos <cravcampos@gmail.com>
2023-12-03 19:06:39 -0500

    ejemplo de uso del ciclo for of

---
+++ 11-iteradores/js/07-app.js
@@ -0,0 +1,13 @@
+// Ciclo for of
+
+// es una version mas simplificada del for tradicional
+// consta de
+// 1. crear lka variable que se va a mostrar
+// 2. of
+// 3. de donde se va a recorrer
+
+let pendientes = ["Tarea", "Comer", "Proyecto", "Estudiar JS"];
+
+for (let pendiente of pendientes) {
+  console.log(pendiente);
+}
