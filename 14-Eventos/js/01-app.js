d3dbe82d30e2abbf135ad46a97aa1a0e87213a65
Victor Campos <cravcampos@gmail.com>
2023-12-16 23:21:11 -0500

    evento para verificar que el documento este listo

---
+++ 14-Eventos/js/01-app.js
@@ -0,0 +1,7 @@
+console.log("Iniciando carga del documento");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Documento listo!!!");
+});
+
+console.log("Ejecutando algo antes de que el documneto este listo");
